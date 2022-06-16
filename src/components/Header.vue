3289277a80a096575f65108d10b74524952ea3f2
abassGarane <thelibertinewreck@gmail.com>
2022-06-16 13:42:09 +0300

    almost done with header hero section

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,22 @@
 <script>
-export default {
-  name:"Header",
-}
+	export default {
+		name: 'Header',
+	};
 </script>
 
 <template>
-  <div class="header">
-    <h3>Header</h3>
-  </div>
+	<div class="header">
+		<div class="logo-box">
+			<img src="../assets/img/logo-white.png" alt="logo" class="logo" />
+		</div>
+		<div class="text-box">
+			<h1 class="heading-primary">
+				<span class="heading-primary_main">Outdoors</span>
+				<span class="heading-primary_sub">Is where life happens</span>
+			</h1>
+			<a href="#" class="btn btn-white btn-animated">Discover Our Tours</a>
+		</div>
+	</div>
 </template>
 
-<style>
-
-</style>
+<style></style>
