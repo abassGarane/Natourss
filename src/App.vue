d7ba2745b8a92224af13c74fc676662527177165
abassGarane <thelibertinewreck@gmail.com>
2022-06-16 09:45:50 +0300

    started on the header component

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script >
+import Header from "./components/Header.vue"
+export default{
+  components:{
+    Header,
+  }
+}
+</script>
+
+<template>
+<Header/>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name:"Header",
+}
+</script>
+
+<template>
+  <div class="header">
+    <h3>Header</h3>
+  </div>
+</template>
+
+<style>
+
+</style>
