c97bfdc0c0f89e9ada78cbad2610b72479847593
abassGarane <thelibertinewreck@gmail.com>
2022-06-17 14:29:18 +0300

    moving css to scss

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,34 @@
 	<Header />
 </template>
 
-<style></style>
+<style>
+	/*
+COLORS:
+
+Light green: #7ed56f
+Medium green: #55c57a
+Dark green: #28b485
+
+*/
+	html {
+		font-size: 62.5%;
+	}
+
+	*,
+	*::after,
+	*::before {
+		margin: 0;
+		padding: 0;
+		box-sizing: inherit;
+	}
+
+	body {
+		font-family: 'Lato', sans-serif;
+		font-weight: 400;
+		/* font-size: 16px; */
+		line-height: 1.7;
+		color: #777;
+		padding: 3rem;
+		box-sizing: border-box;
+	}
+</style>
