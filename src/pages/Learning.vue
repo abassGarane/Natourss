cd8a767306fe2041b1d0229736f8abb36c1cb540
abassGarane <thelibertinewreck@gmail.com>
2022-06-19 12:49:50 +0300

    lessons

---
+++ src/pages/Learning.vue
@@ -0,0 +1,28 @@
+<script>
+	export default {
+		name: 'Learning',
+	};
+</script>
+
+<template>
+	<h1>Learning</h1>
+	<div class="grid_learn">
+		<div class="row">
+			<div class="col-1-2">col-1-2</div>
+			<div class="col-1-2">col-1-2</div>
+		</div>
+		<div class="row">
+			<div class="col-1-3">col-1-3</div>
+			<div class="col-1-3">col-2-3</div>
+			<div class="col-1-3">col-3-3</div>
+		</div>
+		<div class="row">
+			<div class="col-1-3-2">col-1-3</div>
+			<div class="col-2-3">col-2-3</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+	@use '../scss/layouts/grid';
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '../pages/Home.vue';
+import Learning from '../pages/Learning.vue';
 
 const routes = [
 	{
 		path: '/',
 		name: 'home',
 		component: Home,
+	},
+	{
+		path: '/learning',
+		name: 'learning',
+		component: Learning,
 	},
 ];
 
