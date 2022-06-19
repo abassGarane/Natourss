4376bc2b860cdd2c93d85765cb19893a903cccf8
abassGarane <thelibertinewreck@gmail.com>
2022-06-19 13:04:18 +0300

    started on the about section

---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<script>
+	export default {
+		name: 'About',
+	};
+</script>
+
+<template>
+	<section class="about">
+		<h2 class="heading-secondary">Exciting tours for adventurous people</h2>
+	</section>
+</template>
+
+<style lang="scss">
+	@use '../scss/components/about';
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,21 @@
 <script>
 	import Header from '../components/Header.vue';
+	import About from '../components/About.vue';
 
 	export default {
 		name: 'Home',
 		components: {
 			Header,
+			About,
 		},
 	};
 </script>
 
 <template>
 	<Header />
+	<main>
+		<About />
+	</main>
 </template>
 
 <style lang="scss">
